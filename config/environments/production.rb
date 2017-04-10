e064808dd555a467d38543946c0fe431590f9600
Loic Vigneron <loic.vigneron@gmail.com>
2017-04-10 17:42:49 +0200

    Remove deprecation warning for 'config.serve_static_files'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.serve_static_files = true
+  config.public_file_server.enabled = true
   config.log_level = :info
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
